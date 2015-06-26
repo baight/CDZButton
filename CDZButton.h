8648a8d8b65afa134541b788aa8c28e9c48aee7f
baight <cz-cczz@163.com>
2015-06-26 16:30:13 +0800

    添加注释

--- CDZButton.h
+++ CDZButton.h
@@ -13,7 +13,7 @@ typedef enum{
     CBTypeHorizontal = 1,  // image 和 title 水平排列
     CBTypeVertical = 2,    // image 和 title 垂直排列
     CBTypeImageOnly = 3,   // 只显示 image, 其 contentMode 与 button 的一致
-    CBTypeVersaHorizontal = 4
+    CBTypeVersaHorizontal = 4   // title 和 image 水平排列
 }CBType;
 
 @interface CDZButton : UIButton{
@@ -21,9 +21,10 @@ typedef enum{
     UIImage* _normalBackgroundImage;
 }
 
-@property (nonatomic, assign) CBType type;
-@property (nonatomic, assign) CGFloat interval;
-// 将自己state改变时，将父容器的所以子视图 的 state 设置成和自己一样的
+@property (nonatomic, assign) CBType type;         // 类型
+@property (nonatomic, assign) CGFloat interval;    // image 和 title 之间的间距
+
+// 自己state改变时，是否将父容器的所有子视图 的 state 设置成和自己一样的，默认否
 @property (nonatomic, assign) BOOL highlightBrotherViews;
 
 @end
